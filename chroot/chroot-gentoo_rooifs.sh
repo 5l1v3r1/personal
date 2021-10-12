91c3fe617c33bf94e94a3357356b8bdf26d23557
wuseman <wuseman@nr1.nu>
2021-10-12 06:23:45 +0200

    Updated header and small typos

--- chroot/chroot-gentoo_rooifs.sh
+++ chroot/chroot-gentoo_rooifs.sh
@@ -1,44 +1,49 @@
 #!/bin/bash
-################################################################################
-################################################################################
-####                                                                       #####
-#### A notice to all nerds.                                                #####
-#### If you will copy developers real work it will not make you a hacker.  #####
-#### Resepect all developers, we doing this because it's fun!              #####
-####                                                                       #####
-################################################################################
-################################ SOURCE CODE ###################################
-################################################################################
-################ ##### HDDKILLER WAS FOUNDED BY WUSEMAN ########################
-################################################################################
-####                                                                       #####
-####  Chroot - Facilitate the chroot from a livecd/usb                     #####
-####  Copyright (C) 2013-2019, wuseman                                     #####
-####                                                                       #####
-####  This program is free software; you can redistribute it and/or modify #####
-####  it under the terms of the GNU General Public License as published by #####
-####  the Free Software Foundation; either version 2 of the License, or    #####
-####  (at your option) any later version.                                  #####
-####                                                                       #####
-####  This program is distributed in the hope that it will be useful,      #####
-####  but WITHOUT ANY WARRANTY; without even the implied warranty of       #####
-####  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        #####
-####  GNU General Public License for more details.                         #####
-####                                                                       #####
-####  You must obey the GNU General Public License. If you will modify     #####
-####  chroot file(s), you may extend this exception to your version        #####
-####  of the file(s), but you are not obligated to do so.  If you do not   #####
-####  wish to do so, delete this exception statement from your version.    #####
-####  If you delete this exception statement from all source files in the  #####
-####  program, then also delete it here.                                   #####
-####                                                                       #####
-####  Contact:                                                             #####
-####          IRC: Freenode @ wuseman                                      #####
-####          Mail: wuseman <wuseman@nr1.nu>                               #####
-####                                                                       #####
-################################################################################
-#### SCRIPT BEGINS BERE ########################################################
-################################################################################
+
+# - iNFO -----------------------------------------------------------------------------
+#
+#        Author: wuseman <wuseman@nr1.nu>
+#      FileName: chroot.sh
+#       Version: 1.0
+#
+#       Created: 2013-00-00 (00:00:00)
+#      Modified: 2021-09-10 (01:51:09)
+#
+#           iRC: wuseman (Libera/EFnet/LinkNet) 
+#       Website: https://www.nr1.nu/
+#        GitHub: https://github.com/wuseman/
+#
+# - Descrpiption --------------------------------------------------------------------
+#
+#      No description has been added
+#
+# - LiCENSE -------------------------------------------------------------------------
+#
+#      Copyright (C) 2021, wuseman                                     
+#                                                                       
+#      This program is free software; you can redistribute it and/or modify 
+#      it under the terms of the GNU General Public License as published by 
+#      the Free Software Foundation; either version 3 of the License, or    
+#      (at your option) any later version.                                  
+#                                                                       
+#      This program is distributed in the hope that it will be useful,      
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of       
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        
+#      GNU General Public License for more details.                         
+#                                                                       
+#      You must obey the GNU General Public License. If you will modify     
+#      the file(s), you may extend this exception to your version           
+#      of the file(s), but you are not obligated to do so.  If you do not   
+#      wish to do so, delete this exception statement from your version.    
+#      If you delete this exception statement from all source files in the  
+#      program, then also delete it here.                                   
+#
+#      You should have received a copy of the GNU General Public License
+#      along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# - End of Header -------------------------------------------------------------
+
+
 
 banner() {
 echo -e "\e[1;35m "
@@ -95,9 +100,9 @@ ROOT="/dev/sdb3"
 GENTOO="/mnt/gentoo"
 
 printf "Please wait, decrypting $ROOT\n"
-cryptsetup --key-file $MNT/.key_files/thinkpad-w541/root.txt luksOpen $ROOT rootfs
+cryptsetup --key-file $MNT/.key_files/root.txt luksOpen $ROOT rootfs
 printf "Please wait, decrypting home-opt-swap\n"
-cryptsetup --key-file $MNT/.key_files/thinkpad-w541/root.txt luksOpen $HOME home
+cryptsetup --key-file $MNT/.key_files/root.txt luksOpen $HOME home
 
 sleep 1
 vgchange -a y
