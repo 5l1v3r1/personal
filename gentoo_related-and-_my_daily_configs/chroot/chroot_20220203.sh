b5a62d67f3a972842fabfcd336b1be2a1fe48bb1
wuseman <wuseman@nr1.nu>
2022-05-28 23:18:06 +0200

    gentoo_related-and-_my_daily_configs/

---
+++ gentoo_related-and-_my_daily_configs/chroot/chroot_20220203.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# - iNFO -----------------------------------------------------------------------------
+#
+#        Author: wuseman <wuseman@nr1.nu>
+#      FileName: chroot_20220203.sh
+#       Version: 1.0
+#
+#       Created: 2022-02-23 (05:21:09)
+#      Modified: 
+#
+#           iRC: wuseman (Libera/EFnet/LinkNet) 
+#       Website: https://www.nr1.nu/
+#        GitHub: https://github.com/wuseman/
+#
+# - Descrpiption --------------------------------------------------------------------
+#
+#       Recently when I installed Gentoo portage is whining about /run is not
+#       mounted when trying to emerge and configuring things, for example
+#       
+#       emerge --config sys-libs/timezone-data will result in failure, however
+#       use this chroot in future
+#
+# - LiCENSE -------------------------------------------------------------------------
+#
+#      Copyright (C) 2021, wuseman                                     
+#                                                                       
+#      This program is free software; you can redistribute it and/or modify 
+#      it under the terms of the GNU General Public License as published by 
+#      the Free Software Foundation; either version 3 of the License, or    
+#      (at your option) any later version.                                  
+#                                                                       
+#      This program is distributed in the hope that it will be useful,      
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of       
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        
+#      GNU General Public License for more details.                         
+#                                                                       
+#      You must obey the GNU General Public License. If you will modify     
+#      the file(s), you may extend this exception to your version           
+#      of the file(s), but you are not obligated to do so.  If you do not   
+#      wish to do so, delete this exception statement from your version.    
+#      If you delete this exception statement from all source files in the  
+#      program, then also delete it here.                                   
+#
+#      You should have received a copy of the GNU General Public License
+#      along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# - End of Header -------------------------------------------------------------
+
+mount --types proc /proc /mnt/gentoo/proc
+mount --rbind /sys /mnt/gentoo/sys
+mount --make-rslave /mnt/gentoo/sys
+mount --rbind /dev /mnt/gentoo/dev
+mount --make-rslave /mnt/gentoo/dev
+mount --bind /run /mnt/gentoo/run
+mount --make-slave /mnt/gentoo/run 
